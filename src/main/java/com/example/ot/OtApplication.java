cfab4d77fbcada2ffb6914e84bc311f8e9068011
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-05 14:45:33 +0900

    first commit

---
+++ src/main/java/com/example/ot/OtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ot/OtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
