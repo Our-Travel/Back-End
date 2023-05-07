708f2c6b45331d92e16d8ef650a907f314dee001
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-07 22:13:40 +0900

    map과 httpheader 유동적으로

--- src/main/java/com/example/ot/util/Util.java
+++ src/main/java/com/example/ot/util/Util.java
@@ -5,9 +5,27 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Util {
+
+    public static <K, V> Map<K, V> mapOf(Object... args) {
+        Map<K, V> map = new LinkedHashMap<>();
+        int size = args.length / 2;
+
+        for (int i = 0; i < size; i++) {
+            int keyIndex = i * 2;
+            int valueIndex = keyIndex + 1;
+
+            K key = (K) args[keyIndex];
+            V value = (V) args[valueIndex];
+
+            map.put(key, value);
+        }
+        return map;
+    }
+
     public static class spring {
 
         public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData) {
@@ -16,6 +34,19 @@ public class Util {
 
         public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData, HttpHeaders headers) {
             return new ResponseEntity<>(rsData, headers, rsData.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+        }
+
+        public static HttpHeaders httpHeadersOf(String... args) {
+            HttpHeaders headers = new HttpHeaders();
+
+            Map<String, String> map = Util.mapOf(args);
+
+            for (String key : map.keySet()) {
+                String value = map.get(key);
+                headers.set(key, value);
+            }
+
+            return headers;
         }
     }
 }
