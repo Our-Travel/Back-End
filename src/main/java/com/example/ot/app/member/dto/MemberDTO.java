13abf59c3031292c44302dbb69b8bb6ceaeb3f06
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-14 19:53:34 +0900

    [CHORE]스웨거 명명 수정

--- src/main/java/com/example/ot/app/member/dto/MemberDTO.java
+++ src/main/java/com/example/ot/app/member/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.example.ot.app.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -11,15 +12,24 @@ public class MemberDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SignUpDto {
+        @Schema(description = "아이디")
         @NotBlank(message = "아이디를 입력해주세요.")
         @Email
         private String username;
+
+        @Schema(description = "비밀번호")
         @NotBlank(message = "비밀번호를 입력해주세요.")
         private String password;
+
+        @Schema(description = "닉네임")
         @NotBlank(message = "닉네임을 입력해주세요.")
         private String nickName;
+
+        @Schema(description = "ex) 서울특별시, 인천광역시")
         @NotBlank(message = "regionLevel1 을(를) 입력해주세요.")
         private String regionLevel1;
+
+        @Schema(description = "ex) 관악구, 강릉시, 화성시")
         @NotBlank(message = "regionLevel2 을(를) 입력해주세요.")
         private String regionLevel2;
     }
