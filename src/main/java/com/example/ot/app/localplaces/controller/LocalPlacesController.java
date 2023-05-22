1e0feb122ff3d5a3311fd364c5725cb8a6279576
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-22 16:58:12 +0900

    [FEAT]주변 숙소 및 숙소 상세정보 제공

--- src/main/java/com/example/ot/app/localplaces/controller/LocalPlacesController.java
+++ src/main/java/com/example/ot/app/localplaces/controller/LocalPlacesController.java
@@ -3,6 +3,7 @@ package com.example.ot.app.localplaces.controller;
 import com.example.ot.app.api.dto.KakaoApiResponseDTO;
 import com.example.ot.app.api.service.KakaoCategorySearchService;
 import com.example.ot.app.base.rsData.RsData;
+import com.example.ot.app.localplaces.entity.Hotel;
 import com.example.ot.app.localplaces.entity.Spot;
 import com.example.ot.app.localplaces.service.LocalPlacesService;
 import com.example.ot.config.security.entity.MemberContext;
@@ -30,7 +31,7 @@ public class LocalPlacesController {
     private static final String SPOT_CATEGORY = "AT4";
     private static final String HOTEL_CATEGORY = "AD5";
 
-    @Operation(summary = "주변 관광지", security = @SecurityRequirement(name = "bearerAuth"))
+    @Operation(summary = "주변 관광지 제공", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/spot")
     public ResponseEntity<RsData> spot(@AuthenticationPrincipal MemberContext memberContext, @RequestParam double latitude, @RequestParam double longitude) {
         if(localPlacesService.canResponseSpot(latitude, longitude)){
@@ -43,8 +44,25 @@ public class LocalPlacesController {
     @Operation(summary = "하나의 관광지 세부정보", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/spot/{siteId}")
     public ResponseEntity<RsData> spotId(@AuthenticationPrincipal MemberContext memberContext, @PathVariable Long siteId) {
-        Spot spot = localPlacesService.findSpotByid(siteId);
+        Spot spot = localPlacesService.findSpotById(siteId);
         return Util.spring.responseEntityOf(RsData.successOf(spot));
+    }
+
+    @Operation(summary = "주변 숙박 제공", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/hotel")
+    public ResponseEntity<RsData> hotel(@AuthenticationPrincipal MemberContext memberContext, @RequestParam double latitude, @RequestParam double longitude) {
+        if(localPlacesService.canResponseSpot(latitude, longitude)){
+            return Util.spring.responseEntityOf(RsData.of("F-1", "위도 또는 경도값이 없습니다."));
+        }
+        KakaoApiResponseDTO responseDTOList = localPlacesService.response(HOTEL_CATEGORY, latitude, longitude);
+        return Util.spring.responseEntityOf(RsData.successOf(responseDTOList));
+    }
+
+    @Operation(summary = "하나의 숙박 세부정보", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/hotel/{hotelId}")
+    public ResponseEntity<RsData> hotelId(@AuthenticationPrincipal MemberContext memberContext, @PathVariable Long hotelId) {
+        Hotel hotel = localPlacesService.findHotelById(hotelId);
+        return Util.spring.responseEntityOf(RsData.successOf(hotel));
     }
 
 }
--- src/main/java/com/example/ot/app/localplaces/service/LocalPlacesService.java
+++ src/main/java/com/example/ot/app/localplaces/service/LocalPlacesService.java
@@ -3,7 +3,9 @@ package com.example.ot.app.localplaces.service;
 import com.example.ot.app.api.dto.DocumentDTO;
 import com.example.ot.app.api.dto.KakaoApiResponseDTO;
 import com.example.ot.app.api.service.KakaoCategorySearchService;
+import com.example.ot.app.localplaces.entity.Hotel;
 import com.example.ot.app.localplaces.entity.Spot;
+import com.example.ot.app.localplaces.repository.HotelRepository;
 import com.example.ot.app.localplaces.repository.SpotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,11 +21,40 @@ public class LocalPlacesService {
 
     private final KakaoCategorySearchService kakaoCategorySearchService;
     private final SpotRepository spotRepository;
+    private final HotelRepository hotelRepository;
 
-    // 관광지 정보 제공 및 DB저장.
+    // 관광지or숙박 정보 제공 및 DB저장.
     @Transactional
     public KakaoApiResponseDTO response(String category, double latitude, double longitude){
         KakaoApiResponseDTO apiResponseDTO = kakaoCategorySearchService.requestCategorySearch(category, latitude, longitude);
+        if(category.equals("AT4")){
+            saveSpotInfo(apiResponseDTO);
+        }
+        else if(category.equals("AD5")){
+            saveHotelInfo(apiResponseDTO);
+        }
+
+        return kakaoCategorySearchService.requestCategorySearch(category, latitude, longitude);
+    }
+
+    private void saveHotelInfo(KakaoApiResponseDTO apiResponseDTO) {
+        for(DocumentDTO documentDTO : apiResponseDTO.getDocumentDTOList()){
+            if(hotelRepository.existsById(documentDTO.getId())) continue;
+            Hotel hotel = Hotel.builder()
+                    .id(documentDTO.getId())
+                    .addressName(documentDTO.getAddressName())
+                    .placeName(documentDTO.getPlaceName())
+                    .placeUrl(documentDTO.getPlaceUrl())
+                    .phone(documentDTO.getPhone())
+                    .roadAddressName(documentDTO.getRoadAddressName())
+                    .longitude(documentDTO.getLongitude())
+                    .latitude(documentDTO.getLatitude())
+                    .build();
+            hotelRepository.save(hotel);
+        }
+    }
+
+    private void saveSpotInfo(KakaoApiResponseDTO apiResponseDTO) {
         for(DocumentDTO documentDTO : apiResponseDTO.getDocumentDTOList()){
             if(spotRepository.existsById(documentDTO.getId())) continue;
             Spot spot = Spot.builder()
@@ -38,12 +69,14 @@ public class LocalPlacesService {
                     .build();
             spotRepository.save(spot);
         }
-
-        return kakaoCategorySearchService.requestCategorySearch(category, latitude, longitude);
     }
 
-    public Spot findSpotByid(Long siteId) {
+    public Spot findSpotById(Long siteId) {
         return spotRepository.findById(siteId).orElse(null);
+    }
+
+    public Hotel findHotelById(Long hotelId){
+        return hotelRepository.findById(hotelId).orElse(null);
     }
 
     public boolean canResponseSpot(double latitude, double longitude) {
@@ -52,4 +85,5 @@ public class LocalPlacesService {
         }
         return false;
     }
+
 }
