03ad99b88f35c4cbf0bf5dffc185d6fbee8089b3
Jeongmin Lee <dlwogns9807@naver.com>
2023-06-18 18:47:50 +0900

    [FIX]해시태그 키워드 저장 오류 수정

--- src/main/java/com/example/ot/app/host/service/HostService.java
+++ src/main/java/com/example/ot/app/host/service/HostService.java
@@ -1,6 +1,7 @@
 package com.example.ot.app.host.service;
 
 import com.example.ot.app.base.rsData.RsData;
+import com.example.ot.app.hashtag.service.HashTagService;
 import com.example.ot.app.host.dto.HostDTO;
 import com.example.ot.app.host.entity.Host;
 import com.example.ot.app.host.repository.HostRepository;
@@ -20,6 +21,7 @@ public class HostService {
     private final HostRepository hostRepository;
     private final MemberRepository memberRepository;
     private final CityRepository cityRepository;
+    private final HashTagService hashTagService;
 
     @Transactional
     public RsData<Host> createHost(HostDTO.registerHostDTO registerHostDTO, long id){
@@ -27,19 +29,19 @@ public class HostService {
         if(member == null){
             return RsData.of("F-1", "사용자를 찾을 수 없습니다.");
         }
-//        City city = cityRepository.findById(registerHostDTO.getCity()).orElse(null);
-//        if(city == null){
-//            return RsData.of("F-1", "지역을 선택해주세요.");
-//        }
+        City city = cityRepository.findById(registerHostDTO.getCity()).orElse(null);
+        if(city == null){
+            return RsData.of("F-1", "지역을 선택해주세요.");
+        }
         Host host = Host
                 .builder()
                 .introduction(registerHostDTO.getIntroduction())
                 .member(member)
-//                .city(city)
+                .city(city)
                 .build();
         hostRepository.save(host);
         member.setHostAuthority(true);
-
+        hashTagService.applyHashTags(host, registerHostDTO.getHashTag());
         return RsData.of("S-1", "Host 등록이 완료되었습니다.");
     }
 
