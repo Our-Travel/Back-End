26d0440f768676379936d1246de95875ceeeef2c
Jeongmin Lee <dlwogns9807@naver.com>
2023-06-20 19:06:02 +0900

    [FIX]application구조 변경

--- src/main/java/com/example/ot/config/AppConfig.java
+++ src/main/java/com/example/ot/config/AppConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Getter
     private static ApplicationContext context;
-    @Getter
-    private static long MAX_FILE_SIZE;
-    @Getter
-    private static String uploadPath;
+
     @Autowired
     public void setContext(ApplicationContext context) {
         AppConfig.context = context;
     }
 
-    @Value("${custom.file.maxFileSize}")
-    public void setMaxFileSize(long MAX_FILE_SIZE) {
-        AppConfig.MAX_FILE_SIZE = MAX_FILE_SIZE;
-    }
 
-    @Value("${custom.file.genFileDirPath}")
-    public void setUploadPath(String uploadPath) {
-        AppConfig.uploadPath = uploadPath;
-    }
 
     // access토큰 시간 관련 오류 해결.
     // LocalDateTime 직렬화 역직렬화 오류 해결.
