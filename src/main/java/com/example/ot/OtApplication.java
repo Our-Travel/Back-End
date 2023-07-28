bbbbf843d225bcd73b40df248c75630748ffc26b
yeondol <97437403+Min9807@users.noreply.github.com>
2023-07-28 17:16:08 +0900

    [DOC]스웨거 servers

--- src/main/java/com/example/ot/OtApplication.java
+++ src/main/java/com/example/ot/OtApplication.java
@@ -1,5 +1,7 @@
 package com.example.ot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
+@OpenAPIDefinition
 public class OtApplication {
 
     public static void main(String[] args) {
