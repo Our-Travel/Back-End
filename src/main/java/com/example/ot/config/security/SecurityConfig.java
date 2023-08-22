c92b5b034bc4178d236a880f33f037758b46c5d4
yeondol <97437403+Min9807@users.noreply.github.com>
2023-08-22 22:55:36 +0900

    [CHORE]정리

--- src/main/java/com/example/ot/config/security/SecurityConfig.java
+++ src/main/java/com/example/ot/config/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authentication")
