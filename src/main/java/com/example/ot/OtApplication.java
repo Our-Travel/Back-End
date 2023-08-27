bae65cbcc8fc3d2a6809e80ae0a5f136e5129bed
yeondol <97437403+Min9807@users.noreply.github.com>
2023-08-28 00:03:21 +0900

    [FEAT]TravelBoard 기간에 따라 Status 업데이트 스케줄링

--- src/main/java/com/example/ot/OtApplication.java
+++ src/main/java/com/example/ot/OtApplication.java
@@ -1,18 +1,18 @@
 package com.example.ot;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
+@EnableScheduling
 public class OtApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/ot/app/board/repository/TravelBoardRepository.java
+++ src/main/java/com/example/ot/app/board/repository/TravelBoardRepository.java
@@ -1,8 +1,14 @@
 package com.example.ot.app.board.repository;
 
+import com.example.ot.app.board.entity.RecruitmentStatus;
 import com.example.ot.app.board.entity.TravelBoard;
 import com.example.ot.app.board.exception.TravelBoardException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 import static com.example.ot.app.board.exception.ErrorCode.BOARD_NOT_EXISTS;
 
@@ -11,4 +17,12 @@ public interface TravelBoardRepository extends JpaRepository<TravelBoard, Long>,
     default TravelBoard findByBoardId(Long boardId){
         return findById(boardId).orElseThrow(() -> new TravelBoardException(BOARD_NOT_EXISTS));
     }
+
+    @Modifying
+    @Query("UPDATE TravelBoard t SET t.recruitmentStatus = :newStatus WHERE t.recruitmentStatus = :currentStatus AND t.recruitmentPeriodStart = :currentDate")
+    void updateStatusToOpenForToday(@Param("currentStatus") RecruitmentStatus currentStatus, @Param("newStatus") RecruitmentStatus newStatus, @Param("currentDate") LocalDate currentDate);
+
+    @Modifying
+    @Query("UPDATE TravelBoard t SET t.recruitmentStatus = :newStatus WHERE t.recruitmentStatus = :currentStatus AND t.recruitmentPeriodEnd = :currentDate")
+    void updateStatusOnRecruitmentPeriodEnd(@Param("currentStatus") RecruitmentStatus currentStatus, @Param("newStatus") RecruitmentStatus newStatus, @Param("currentDate") LocalDate currentDate);
 }
