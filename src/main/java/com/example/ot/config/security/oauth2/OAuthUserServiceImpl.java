b557ca4592459e2d10fd52926742b4914ae8a65b
yeondol <97437403+Min9807@users.noreply.github.com>
2023-09-18 17:05:19 +0900

    [FIX]네이버 oauth2 형식 수정

--- src/main/java/com/example/ot/config/security/oauth2/OAuthUserServiceImpl.java
+++ src/main/java/com/example/ot/config/security/oauth2/OAuthUserServiceImpl.java
@@ -13,6 +13,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static com.example.ot.app.member.exception.ErrorCode.USERNAME_NOT_EXISTS;
 
 @RequiredArgsConstructor
@@ -26,11 +29,18 @@ public class OAuthUserServiceImpl extends DefaultOAuth2UserService {
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
         String providerType = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
-        String providerTypeCode = userRequest.getClientRegistration().getRegistrationId().toUpperCase().split("")[0];
-        String oauthId = oAuth2User.getName().substring(0, 6);
-
+        String oauthId = "";
+        String providerTypeCode = "";
+        providerTypeCode = userRequest.getClientRegistration().getRegistrationId().toUpperCase().split("")[0];
+        if(providerType.equals("NAVER")){
+            Map<String, Object> map = oAuth2User.getAttributes();
+            LinkedHashMap<String, String> getNaverId = (LinkedHashMap<String, String>) map.get("response");
+            oauthId = getNaverId.get("id");
+        }
+        else{
+            oauthId = oAuth2User.getName().substring(0, 6);
+        }
         Member member = saveOrGetMember(oauthId, providerTypeCode, providerType);
         memberService.genAccessToken(member);
         return new CustomOAuth2User(member.getUsername(), member.getNickName(), member.getAuthorities());
