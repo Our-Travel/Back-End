f0ded2a77ead2b8353b21c6851521528f8ee6cce
yeondol <97437403+Min9807@users.noreply.github.com>
2023-10-06 23:42:32 +0900

    [FIX]채팅 알림 메시지 admin으로 변경

--- src/main/java/com/example/ot/app/chat/dto/request/MessageRequest.java
+++ src/main/java/com/example/ot/app/chat/dto/request/MessageRequest.java
@@ -2,11 +2,13 @@ package com.example.ot.app.chat.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class MessageRequest {
@@ -15,16 +17,11 @@ public class MessageRequest {
     private String message;
     private String writerNickname;
 
-    private MessageRequest(Long roomId, String noticeMessage){
-        this.roomId = roomId;
-        this.message = noticeMessage;
-    }
-
     public static MessageRequest CreateEnterMessage(Long roomId, String noticeMessage){
-        return new MessageRequest(roomId, noticeMessage);
+        return new MessageRequest(roomId, noticeMessage, "admin");
     }
 
     public static MessageRequest CreateExitMessage(Long roomId, String noticeMessage){
-        return new MessageRequest(roomId, noticeMessage);
+        return new MessageRequest(roomId, noticeMessage, "admin");
     }
 }
