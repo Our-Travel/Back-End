1005292b27845cbe85fbe8f293175de69e24e00b
yeondol <97437403+Min9807@users.noreply.github.com>
2023-10-28 18:24:37 +0900

    [TEST]게시물 벌크 인서트 시간 추가.

--- src/test/java/com/example/ot/app/board/bulk/BoardBulkInsertTest.java
+++ src/test/java/com/example/ot/app/board/bulk/BoardBulkInsertTest.java
@@ -2,18 +2,19 @@ package com.example.ot.app.board.bulk;
 
 import com.example.ot.app.board.entity.TravelBoard;
 import com.example.ot.app.board.repository.TravelBoardRepository;
-import com.example.ot.app.member.entity.Member;
-import com.example.ot.app.member.repository.MemberRepository;
 import com.example.ot.util.BoardFixtureFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.StopWatch;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
 public class BoardBulkInsertTest {
@@ -25,10 +26,23 @@ public class BoardBulkInsertTest {
     void bulkInsert() {
         EasyRandom board = BoardFixtureFactory.getBoard();
 
-        List<TravelBoard> travelBoardList = IntStream.range(0, 5)
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        List<TravelBoard> travelBoardList = IntStream.range(0, 10000)
+                .parallel()
                 .mapToObj(i -> board.nextObject(TravelBoard.class))
                 .toList();
 
+        stopWatch.stop();
+        log.info("객체 생성 시간 : {}", stopWatch.getTotalTimeSeconds());
+
+        StopWatch queryStopWatch = new StopWatch();
+        queryStopWatch.start();
+
         travelBoardRepository.bulkInsert(travelBoardList);
+
+        queryStopWatch.stop();
+        log.info("DB Insert 시간 : {}", queryStopWatch.getTotalTimeSeconds());
     }
 }
